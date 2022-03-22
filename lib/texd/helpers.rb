f0965b3fb49e4ab78c3d5f34c86502a39ab311bd
Dominik Menke <dom@digineo.de>
2022-03-22 14:49:46 +0100

    early return for Texd::Helper#escape

--- lib/texd/helpers.rb
+++ lib/texd/helpers.rb
@@ -28,8 +28,9 @@ module Texd
 
     # Escapes the given text, making it safe for use in TeX documents.
     def escape(text, line_break = "\\\\\\", typographic: true)
-      text = +text.to_s # text might be nil or a frozen string
-      text.tap do |str|
+      return "" if text.blank?
+
+      text.to_s.dup.tap do |str|
         str.gsub!(ESCAPE_RE) do |m|
           if Regexp.last_match(1)
             "\\#{m}"
